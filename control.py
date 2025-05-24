9fbd81e96e7633f72c790bab192d9496129d8f61
makiroll.mark <delacruzmacky39@gmail.com>
2025-05-24 18:53:55 +0800

    new

---
+++ control.py
@@ -0,0 +1,16 @@
+#trying an auto clicker woth python
+
+
+import pyautogui
+import keyboard 
+import time
+
+#ctrl /
+pyautogui.moveTo(100, 500, duration=0.5)
+
+#time.sleep(2)
+
+pyautogui.moveTo(500, None, duration=0.5)
+
+pyautogui.click()
+
